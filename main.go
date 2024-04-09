bca8164462b6b335d6ec9d229c0aa60a769c1e93
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-04-09 11:37:03 -0700

    flappy telegram bot

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("7181024480:AAF1_1hvuUr3LfPUis4omoVZZXO80GCjTdk")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 600
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message{
+			if strings.Contains("@catty_flappy_bot", update.Message.Text) {
+				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				msg.ReplyToMessageID = update.Message.MessageID
+				bot.Send(msg)
+			}
+		}
+	}
+}
